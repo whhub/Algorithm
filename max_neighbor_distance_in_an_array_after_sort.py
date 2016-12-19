11165cf8e674418778f9395ceb9fefb6bce64e8d
whhub <wanghui122004@me.com>
2016-12-19 23:21:13 +0800

    [Algorithms][python][0005] 无序数组排序后的最大相邻差值 nose ut

---
+++ max_neighbor_distance_in_an_array_after_sort.py
@@ -0,0 +1,9 @@
+"""
+#coding=utf8
+无序数组排序后的最大相邻差值
+"""
+
+
+def calculate(num_list):
+    return 3
+
---
+++ max_neighbor_distance_in_an_array_after_sort_ut.py
@@ -0,0 +1,20 @@
+"""
+#coding=utf8
+无序数组排序后的最大相邻差值 unit test
+"""
+
+# TODO: how to use nose as an unit test framework
+
+from nose.tools import *
+from max_neighbor_distance_in_an_array_after_sort import calculate
+
+def test_max_neighbor_distance_in_an_array_after_sort():
+    # Arrange
+    num_list = [2, 6, 3, 4, 5, 10, 9]
+    # Act
+
+    actual = calculate(num_list)
+    # Assert
+    assert_equal(4, actual)
+
+
