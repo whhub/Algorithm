68a1bb707fc2c98b15089b6b70e385459798f11c
whhub <wanghui122004@me.com>
2016-12-19 22:23:00 +0800

    [Algorithms][python][0004] RandomList Generator - change to use random.sample

--- RandomList.py
+++ RandomList.py
@@ -7,9 +7,7 @@ import random
 
 def random_list():
     count = random.randint(10, 100)
-    num_list = []
-    for i in range(0, count):
-        num_list.append(random.randint(0, 100))
+    num_list = random.sample(range(100), count)
     return num_list
 
 
