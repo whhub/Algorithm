8f5d6f3f8a9118406902685bbb481f1fa2ea272c
whhub <wanghui122004@me.com>
2016-12-22 22:55:51 +0800

    [Algorithms][python][0007][max_neighbor_distance_in_an_array] Don't know how to tell computer to find left_most_non_empty_bucket & right_most_non_empty_bucket

--- max_neighbor_distance_in_an_array_after_sort.py
+++ max_neighbor_distance_in_an_array_after_sort.py
@@ -5,5 +5,21 @@
 
 
 def calculate(num_list):
+    list_len = len(num_list)
+    assert list_len > 0
+
+    bucket_len = (max(num_list) - min(num_list)) / list_len + 1
+    bucket = [[]] * bucket_len
+
+    # put numbers in bucket based on their size
+    for i in range(list_len):
+        current = num_list[i]
+        j = current / bucket_len
+        bucket[j].append(current)
+
+    # TODO: How to tell computer to find left_most_non_empty_bucket & right_most_non_empty_bucket
+
     return 3
+
+
 
