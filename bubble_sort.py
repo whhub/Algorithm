63c22e7912a636a0dbd9918b11a8b6eca62a0ca9
whhub <wanghui122004@me.com>
2016-12-24 22:50:56 +0800

    [Algorithms][python][0009] cocktail_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,13 @@
 from RandomList import random_list
 
 
-def bubble_sort(nums):
-    n = len(nums)
+def bubble_sort(a):
+    n = len(a)
     for i in range(n-1):
         for j in range(n-1-i):
-            if nums[j] > nums[j+1]:
-                nums[j], nums[j+1] = nums[j+1], nums[j]
-    return nums
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+    return a
 
 rand_nums = random_list()
 print("Before sort: ", rand_nums)
