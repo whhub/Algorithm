0a9fd4b9a0489a93e0358e02f1fc71387df66bb6
whhub <wanghui122004@me.com>
2016-12-25 11:30:16 +0800

    [Algorithms][python][0012] cocktail_sort sum of count of compare and exchange

--- cocktail_sort.py
+++ cocktail_sort.py
@@ -2,17 +2,26 @@ from RandomList import random_list
 
 
 def cocktail_sort(a):
+    compare_count = 0
+    exchange_count = 0
     n = len(a)
     for i in range(int(n / 2)):
         for j in range(i, n-1-i):
+            compare_count += 1
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
+                exchange_count += 1
         for j in range(n-1-i, i, -1):
+            compare_count += 1
             if a[j-1] > a[j]:
                 a[j-1], a[j] = a[j], a[j-1]
+                exchange_count += 1
+    print("Compared: %d, Exchanged: %d" % (compare_count, exchange_count))
     return a
 
+
 rand_nums = random_list()
-print("Before sort: ", rand_nums)
+print("Before sort: ", len(rand_nums), rand_nums)
 cocktail_sort(rand_nums)
-print("After cocktail sort: ", rand_nums)
+print("After Bubble sort: ", len(rand_nums), rand_nums)
+
