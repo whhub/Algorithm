aceac9b67ca1a2de218a587302bc9ea3917790af
whhub <wanghui122004@me.com>
2016-12-25 11:24:30 +0800

    [Algorithms][python][0011] bubble_sort sum of count of compare and exchange

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,17 +3,22 @@ from RandomList import random_list
 
 def bubble_sort(a):
     did_swap = False
+    compare_count = 0
+    exchange_count = 0
     n = len(a)
     for i in range(n-1):
         for j in range(n-1-i):
+            compare_count += 1
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
+                exchange_count +=1
                 did_swap = True
         if not did_swap:
             break
+    print("Compared : %d, Exchanged: %d" % (compare_count, exchange_count))
     return a
 
 rand_nums = random_list()
-print("Before sort: ", rand_nums)
+print("Before sort: ", len(rand_nums), rand_nums)
 bubble_sort(rand_nums)
-print("After Bubble sort: ", rand_nums)
+print("After Bubble sort: ", len(rand_nums), rand_nums)
