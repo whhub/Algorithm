98334b1fb1e5fe2f4f7eacd02bb0a49b8b103063
whhub <wanghui122004@me.com>
2016-12-25 11:16:49 +0800

    [Algorithms][python][0010] bubble_sort 已经排好序之后，及时退出

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,11 +2,15 @@ from RandomList import random_list
 
 
 def bubble_sort(a):
+    did_swap = False
     n = len(a)
     for i in range(n-1):
         for j in range(n-1-i):
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
+                did_swap = True
+        if not did_swap:
+            break
     return a
 
 rand_nums = random_list()
